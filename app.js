b00b4b1d8a0b11523dff22be0e271d8f7d95ac20
souhaila23 <souhailaelazreg@gmail.com>
2020-12-13 10:04:18 +0100

    commit2

--- app.js
+++ app.js
@@ -4,15 +4,20 @@
   angular.module('DIApp', [])
   .controller('DIController', DIController);
 
-  function DIController($scope, $filter){
+  function DIController($scope,
+                        $filter,
+                        $injector){
   $scope.name = "souhaila";
 
   $scope.upper = function () {
     var upCase = $filter('uppercase');
     $scope.name = upCase($scope.name);
-
   };
+console.log($injector.annotate(DIController));
 
   }
-
+  function annonateMe(name, job, blah) {
+    return "Blah!";
+  }
+console.log(annonateMe.toString();
 })()
